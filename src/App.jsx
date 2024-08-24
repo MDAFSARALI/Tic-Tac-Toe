b0b784ebabafa876dd275bf710f6b793cdcd1019
MDAFSARALI <generaluserknown@gmail.com>
2024-08-24 21:37:59 +0530

    Tic-Tac-Tor

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Grid from './components/Grid/Grid'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Grid numberOfCards={9}></Grid>
+    </>
+  )
+}
+
+export default App
